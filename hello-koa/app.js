23ced561be2a6e3a17761132ba3eb293e4f5cd00
unknown <amy.liao@dfrobot.com>
2018-07-27 14:39:49 +0800

    fix koa url chapter problem

--- hello-koa/app.js
+++ hello-koa/app.js
@@ -1,7 +1,5 @@
 // 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
 const Koa = require('koa');
-// 注意require('koa-router')返回的是函数:
-const router = require('koa-router')();
 const bodyParser = require('koa-bodyparser');
 const controller = require('./controller');
 
--- scripts/nodetest.js
+++ scripts/nodetest.js
@@ -20,36 +20,30 @@ var server = http.createServer(function (request, response) {
             console.log('200 ' + request.url);
             response.writeHead(200);
             fs.createReadStream(filepath).pipe(response);         
-        } 
+        }    
         else if (!err_1 && stats.isDirectory())
         {           
-            var filepathDefault = path.join(filepath + 'index.html');
-            console.log('filepathDefault1: ' + filepathDefault);//可以读出filepathDefault:D:\test-site\index.html
-            fs.stat(filepathDefault, function (err_2, stat){                            
-                if(!err_2)
+            var filepathDefault = path.join(filepath , 'index.html');
+            fs.stat(filepathDefault, function (err, stats){                            
+                if(!err && stats.isFile()){                                      
+                    var filepathDefault = path.join(filepath , 'index.html');//否则不知道为什么filepathDefault变为undefined
+                    console.log('200 ' + request.url);
+                    response.writeHead(200);
+                    fs.createReadStream(filepathDefault).pipe(response);
+                    }                               
+                else
                 {
-                    console.log('filepathDefault2: ' + filepathDefault);//filepathDefault变为undefined
-                    if(stat.isFile())
-                    {                 
-                        console.log(stat);
-                        console.log('200 ' + request.url);
-                        response.writeHead(200);
-                        fs.createReadStream(filepathDefault);//error 因为filepathDefault是undefined
-                        //fs.createReadStream(filepathDefault).pipe(response);//error
-                    }
-                    else
-                    {
-                        var filepathDefault =path.join(filepath + 'default.html');
-                        fs.stat(filepathDefault, function (err_3, stat){
-                            if(!err_3 && stat.isFile())
-                            {
-                                console.log('200 ' + request.url);
-                                response.writeHead(200);
-                                fs.createReadStream(filepathDefault).pipe(response);
-                            }
-                        })
-                    }
-                }    
+                    var filepathDefault =path.join(filepath + 'default.html');
+                    fs.stat(filepathDefault, function (err, stats){
+                        if(!err && stats.isFile())
+                        {
+                            //var filepathDefault =path.join(filepath + 'default.html');//这里倒不需要重新赋值
+                            console.log('200 ' + request.url);
+                            response.writeHead(200);
+                            fs.createReadStream(filepathDefault).pipe(response);
+                        }
+                    })
+                } 
             })
         } 
         else 
